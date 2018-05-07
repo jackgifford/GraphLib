c30995a306ac1ee08f99c80ebc153969cf80a434
Jack Gifford <shmacktus@gmail.com>
2018-05-07 21:00:57 +1000

    Initial commit

---
+++ GraphLib/Models/IGraph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GraphLib.Models
+{
+    interface IGraph<T>
+    {
+        void CreateEdge(T initial, T final);
+        void RemoveEdge(T initial, T final);
+        bool Adjacent(T initial, T final);
+
+    }
+}
